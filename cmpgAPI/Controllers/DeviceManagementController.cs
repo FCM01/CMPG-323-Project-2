d69c254afce97cf62253604ef987a779927185e0
FCM01 <faraimatyukira1@gmail.com>
2022-09-01 18:56:57 +0200

    Add project files.

---
+++ cmpgAPI/Controllers/DeviceManagementController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using cmpgAPI.models;
+
+namespace cmpgAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeviceManagementController : ControllerBase
+    {
+        private readonly ConnectedOfficeContext _context;
+
+        public DeviceManagementController(ConnectedOfficeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/DeviceManagement
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Device>>> GetDevice()
+        {
+            return await _context.Device.ToListAsync();
+        }
+
+        // GET: api/DeviceManagement/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Device>> GetDevice(Guid id)
+        {
+            var device = await _context.Device.FindAsync(id);
+
+            if (device == null)
+            {
+                return NotFound();
+            }
+
+            return device;
+        }
+
+        // PUT: api/DeviceManagement/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDevice(Guid id, Device device)
+        {
+            if (id != device.DeviceId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(device).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DeviceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/DeviceManagement
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Device>> PostDevice(Device device)
+        {
+            _context.Device.Add(device);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (DeviceExists(device.DeviceId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetDevice", new { id = device.DeviceId }, device);
+        }
+
+        // DELETE: api/DeviceManagement/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Device>> DeleteDevice(Guid id)
+        {
+            var device = await _context.Device.FindAsync(id);
+            if (device == null)
+            {
+                return NotFound();
+            }
+
+            _context.Device.Remove(device);
+            await _context.SaveChangesAsync();
+
+            return device;
+        }
+
+        private bool DeviceExists(Guid id)
+        {
+            return _context.Device.Any(e => e.DeviceId == id);
+        }
+    }
+}
